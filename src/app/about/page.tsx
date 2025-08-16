012b46c233d228a9f4b7b315071af59056d4ebec
yousefnajjar <yousefnajjar996@gmail.com>
2025-08-16 16:59:05 +0300

    keywords

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,15 @@
 import AboutSectionOne from "@/components/About/AboutSectionOne";
 import AboutSectionTwo from "@/components/About/AboutSectionTwo";
 import Breadcrumb from "@/components/Common/Breadcrumb";
-
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "About Us",
   description: "",
   // other metadata
+  keywords: [
+
+  ],
 };
 
 const AboutPage = () => {
