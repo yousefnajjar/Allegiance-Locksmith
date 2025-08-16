dc3351b5cb1ba76d933630f16c56372cea292264
yousefnajjar <yousefnajjar996@gmail.com>
2025-08-16 15:21:51 +0300

    reurn about us api

--- src/app/api/aboutus/route.ts
+++ src/app/api/aboutus/route.ts
@@ -1,16 +1,16 @@
-// import pool from "@/app/lib/db";
-// export async function GET() {
-//   try {
-//     const client = await pool.connect();
-//     const result = await client.query("SELECT * FROM about_us");
-//     client.release();
-//     console.log("Database query successful:", result.rows);
-//     return new Response(JSON.stringify(result.rows), {
-//       status: 200,
-//       headers: { "Content-Type": "application/json" },
-//     });
-//   } catch (error) {
-//     console.error("Database query error:", error);
-//     return new Response("Internal Server Error", { status: 500 });
-//   }
-// }
+import pool from "@/app/lib/db";
+export async function GET() {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM about_us");
+    client.release();
+    console.log("Database query successful:", result.rows);
+    return new Response(JSON.stringify(result.rows), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Database query error:", error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
+}
