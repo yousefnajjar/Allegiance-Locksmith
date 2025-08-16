d7495ce6423d2bf1589ed9500a82d74dd5b97620
yousefnajjar <yousefnajjar996@gmail.com>
2025-08-16 18:37:13 +0300

    update icons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -57,14 +57,14 @@ const Header = () => {
                 } `}
               >
                 <Image
-                  src="/images/logo/logo.png"
+                  src="/images/logo/Allegiance Locksmith 1.png"
                   alt="logo"
                   width={140}
                   height={30}
                   className="w-full dark:hidden"
                 />
                 <Image
-                  src="/images/logo/logo.png"
+                  src="/images/logo/Allegiance Locksmith 2.png"
                   alt="logo"
                   width={140}
                   height={30}
